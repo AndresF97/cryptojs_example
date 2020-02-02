97d743aec9134951d92d3acd08cb73ee89368ff9
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-02-02 00:04:39 -0800

    deledted some testing text and got the decrypt data to display rigth

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 $("form").on("submit",function(event){
     event.preventDefault()
-    console.log($("#emailInput").val())
-    //console.log($("#passwordInput").val())
     var user = $("#emailInput").val()
     var password = $("#passwordInput").val();
     console.log(password)
